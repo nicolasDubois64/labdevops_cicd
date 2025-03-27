e2148586eac4ffd3e85691a4ad28751a0da5cf72
nicolasDubois64 <nicolas.dubois64@gmail.com>
2025-03-27 11:48:32 +0100

    add db test

---
+++ test/util/database.test.ts
@@ -0,0 +1,21 @@
+import ComputeUtil from "../../src/util/compute.util";
+import { Client } from "pg";
+
+describe("Database", () => {
+  test("connection", async () => {
+    const database = new Client({
+      host: "127.0.0.1",
+      port: 5432,
+      database: "cicd_database",
+      user: "cicd_user",
+      password: "cicd_password",
+    });
+
+    const connected = await database
+      .connect()
+      .then(() => true)
+      .catch(() => false)
+      .finally(async () => await database.end());
+    expect(connected).toBe(true);
+  });
+});
